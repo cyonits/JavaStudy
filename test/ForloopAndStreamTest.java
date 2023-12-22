4b0368424ba3fdc0d54e545a858783d008c05f18
cyonits <cyonitss@gmail.com>
2023-12-22 15:21:22 +0900

    commit4 for, stream test

--- test/ForloopAndStreamTest.java
+++ test/ForloopAndStreamTest.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -14,86 +15,83 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ForloopAndStreamTest {
 
     static List<Integer> nums = new ArrayList<>();
-    Integer sum;
 
     @BeforeAll
     public static void makeNumList(){
         nums .clear();
-        for (int i = 0; i < 100000000; i++) {
+        for (int i = 0; i < 5000000; i++) {
             nums.add(i);
         }
     }
 
-    @BeforeEach
-    public void makeSumZero(){
-        sum = 0;
+    @Test
+    public void a_init() throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+
+        Thread.sleep(5000);
+
+        long endTime = System.currentTimeMillis();
+        long durationTimeSec = endTime - startTime;
+        System.out.println("init Time : " + durationTimeSec);
     }
 
     @Test
-    public void a_init(){
-    }
-
-    @Test
-    public void b_ForloopTest() {
+    public void c_ForloopTest() {
+        List<String> strings = new ArrayList<>();
         long startTime = System.currentTimeMillis();
 
         for (int i = 0; i < nums.size(); i++) {
-            sum += nums.get(i);
-            if(sum < 1000000000){
-                sum = 0;
-            }
+            strings.add(nums.get(i) + UUID.randomUUID().toString());
         }
 
         long endTime = System.currentTimeMillis();
         long durationTimeSec = endTime - startTime;
         System.out.println("For-loop Time : " + durationTimeSec);
+        strings.clear();
     }
 
     @Test
-    public void c_ForeachTest() {
+    public void d_ForeachTest() {
+        List<String> strings = new ArrayList<>();
         long startTime = System.currentTimeMillis();
 
-        for (int i: nums) {
-            sum += nums.get(i);
-            if(sum < 1000000000){
-                sum = 0;
-            }
+        for (Integer i: nums) {
+            strings.add(nums.get(i) + UUID.randomUUID().toString());
         }
 
         long endTime = System.currentTimeMillis();
         long durationTimeSec = endTime - startTime;
         System.out.println("For-each Time : " + durationTimeSec);
+        strings.clear();
     }
 
     @Test
-    public void d_StreamTest() {
+    public void e_StreamTest() {
+        List<String> strings = new ArrayList<>();
         long startTime = System.currentTimeMillis();
 
         nums.forEach(num -> {
-            sum += num;
-            if(sum < 1000000000){
-                sum = 0;
-            }
+            strings.add(nums.get(num) + UUID.randomUUID().toString());
         });
 
         long endTime = System.currentTimeMillis();
         long durationTimeSec = endTime - startTime;
         System.out.println("Stream.foreach Time : " + durationTimeSec);
+        strings.clear();
     }
 
     @Test
-    public void e_IntStreamTest() {
+    public void f_IntStreamTest() {
+        List<String> strings = new ArrayList<>();
         long startTime = System.currentTimeMillis();
 
         IntStream.range(0, nums.size()).forEach(i ->{
-            sum += nums.get(i);
-            if(sum < 1000000000){
-                sum = 0;
-            }
+            strings.add(nums.get(i) + UUID.randomUUID().toString());
         });
 
         long endTime = System.currentTimeMillis();
         long durationTimeSec = endTime - startTime;
         System.out.println("Intstream.foreach Time : " + durationTimeSec);
+        strings.clear();
     }
 }
